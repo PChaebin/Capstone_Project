8a7e351c21d7540c6b3fb97b018aa4efba37f78c
PChaebin <pcd0299@gmail.com>
2024-11-13 01:27:25 +0900

    24.11.13
    - 엫

--- CollegeStudentGrow/Assets/Scripts/PartTimeJobMiniGame.cs
+++ CollegeStudentGrow/Assets/Scripts/PartTimeJobMiniGame.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PartTimeJobMiniGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [System.Serializable]
+    public class Drinks
     {
-        
+        public string Name;
+        public string FinalImage;
+        public List<string> steps;
+        public string Description;
     }
 
-    // Update is called once per frame
-    void Update()
+    [System.Serializable]
+    public class DrinksList
     {
-        
+        public List<Drinks> drinksList;
+    }
+
+    [SerializeField] private Button startButton;
+    [SerializeField] private GameObject recipeUI;
+
+    private DrinksList drinks;
+
+    private float recipedisplayTime = 30f;
+
+    private void Start()
+    {
+        LoadRecipesToJson();
+    }
+
+    private void LoadRecipesToJson()
+    {
+        TextAsset jsonFile = Resources.Load<TextAsset>("Json/CafeRecipe");
+
+        if(jsonFile != null)
+        {
+            drinks = JsonUtility.FromJson<DrinksList>(jsonFile.text);
+        }
+    }
+
+    public void gmaeStatus()
+    {
+        startButton.gameObject.SetActive(false);
+
+        recipeUI.SetActive(true);
+
+        StartCoroutine(DisplayRecipes());
+    }
+
+    private IEnumerator DisplayRecipes()
+    {
+        yield return new WaitForSeconds(recipedisplayTime);
+
+        recipeUI.SetActive(false);
     }
 }
